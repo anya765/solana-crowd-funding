c3e039a96b2a9dbfb0498a321e7014de30b2b147
Anya Singh <anyasingh070605@gmail.com>
2021-12-06 12:24:46 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
       </div>
       <Header setRoute={setRoute} />
       {route === 0 ?
-        <div style ={{columns:'2 auto'}}>{
+        <div style ={{columns:'2 auto', width: '100%', height: '50%', objectFit:'cover'}}>{
           cards.map((e, idx) => (
             <Card
               key={e.pubId.toString()}
